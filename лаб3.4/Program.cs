993e7cc93531e863b5c2730b6f1fd8166f013c39
RobotComp.ru <RobotComp.ru@DESKTOP-94TEC7C>
2024-11-07 22:08:59 +0300

    Добавьте файлы проекта.

---
+++ лаб3.4/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+        double x = 0.1;
+       
+        double endX = 2.1;
+      
+        double step = 0.1;
+
+        Console.WriteLine("xtty");
+        Console.WriteLine("---------------------");
+
+       
+        while (x <= endX)
+        {
+            
+            double y = Math.Pow(x, 2) - x * Math.PI * Math.Cos(Math.PI * x);
+
+           
+            Console.WriteLine($"{x:F1}tt{y:F4}");
+
+           
+            x += step;
+        }
+    }
+}
